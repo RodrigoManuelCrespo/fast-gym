3dfad8240dd85248fe347e4ad840acc07be0bd16
Juliana Battion <julibattion@gmail.com>
2025-06-30 20:17:00 -0300

    Pantallas entrenador/cliente

--- src/app/admin/entrenadores/layout.tsx
+++
@@ -1,8 +0,0 @@
-// src/app/entrenadores/layout.tsx
-"use client"
-
-import DashboardLayout from "@/components/shared/DashboardLayout"
-
-export default function AdminLayout({ children }: { children: React.ReactNode }) {
-    return <DashboardLayout role="entrenador">{children}</DashboardLayout>
-}
